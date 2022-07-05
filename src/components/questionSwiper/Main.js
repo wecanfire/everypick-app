796e5b2830cd362eb3f4d614f8dd51ed1a42e8c5
grasshoppertrainer <grasshoppertrainer@gmail.com>
2022-07-05 23:02:26 +0900

    infinite swiper 테스트

--- src/components/questionSwiper/Main.js
+++ src/components/questionSwiper/Main.js
@@ -2,57 +2,67 @@ import React from 'react';
 import {Text, View, StyleSheet, TextComponent, Vibration} from 'react-native';
 import {StatusBar, Dimensions} from 'react-native';
 import Swiper from 'react-native-swiper';
-import {gestureHandlerRootHOC} from "react-native-gesture-handler";
+import PagerView from 'react-native-pager-view';
 
 import QuestionMain from '../questionPage/Main';
 import CommentList from '../commentPage/Main';
 import questions from "../../assets/question_data.json";
 
-// {/*// /!*<Swiper*!/*/}
-// {/*// /!*  style={styles.swiper}*!/*/}
-// {/*// /!*  horizontal={false}*!/*/}
-// {/*// /!*  showsButtons={true}*!/*/}
-// {/*// /!*  onIndexChanged={onIndexChangedHandle}*!/*/}
-// {/*// /!*  >*!/*/}
-// {/*// /!*{questions.map((question, i) => (*!/*/}
-// {/*// /!*  <View style={{flex: 1}}>*!/*/}
-// {/*// /!*    <QuestionMain style={styles.main}*!/*/}
-// {/*// /!*      bottomSheetModalRef={bottomSheetModalRef}*!/*/}
-// {/*// /!*      questionData={question}>*!/*/}
-// {/*// /!*    </QuestionMain>*!/*/}
-// {/*// /!*  </View>*!/*/}
-// {/*// /!*))}*!/*/}
-// {/*//*/}
-// {/*// /!*</Swiper>*!/*/}
-// {/*// /!*<CommentMain bottomSheetModalRef={bottomSheetModalRef} commentsData={commentsData} />*!/*/}
-//
-
 
 const Main = () => {
     // 임시 데이터 여기서 받아올 것
-    const questions = require('../../assets/question_data.json');
+    const [questions, setQuestions] = React.useState(require('../../assets/question_data.json'));
     const commentsBundles = require('../../assets/comment_data.json');
 
-    // Modal 은 Swiper 밖에 있음.
+    // ref 생성
     const bottomSheetModalRef = React.useRef()
+
+    const que = {
+        uuid: "1",
+        writer: "taejin.kim",
+        create_date: "2021-04-25 05:05:05.55555",
+        update_date: "2021-04-25 05:05:05.55555",
+        question: "question 3",
+        hashtags: [
+            "tag1"
+        ],
+        answers: [
+            {
+                answer: "어쩔티비",
+                count: "1245"
+            },
+            {
+                answer: "저쩔티비",
+                count: "1300"
+            }
+        ],
+        like_count: 32,
+        comment_count: 43,
+        share_count: 23
+    }
 
     // comment 컴포넌트에서 state setter 취득
     var commentSetter;
     const onCommentMount = (setter) => {
         commentSetter = setter
     }
-    // Swipe 시 인덱스 반환하면 callback. 이를 이용해 commentsData 를 업데이트 한다
-    const onIndexChangedHandle = (index) => {
-        if (commentSetter !== 'undefined') {
-            commentSetter(commentsBundles[index])
+
+    // 페이지 전환시 컬백
+    const onPageSelected = (e) => {
+        // 마지막 질문 도달시 z
+        let pos = e.nativeEvent.position
+        if (pos === questions.length - 1) {
+            setQuestions([...questions, {...que, question: `question ${pos}`}])
         }
     }
 
     return (
         <View style={{flex: 1}}>
-            <Swiper
-                horizontal={false}
-                onIndexChanged={onIndexChangedHandle}
+            <PagerView
+                style={{flex: 1}}
+                initialPage={0}
+                orientation={"vertical"}
+                onPageSelected={onPageSelected}
             >
                 {questions.map((question, i) => (
                     <View
@@ -67,7 +77,7 @@ const Main = () => {
                         </QuestionMain>
                     </View>
                 ))}
-            </Swiper>
+            </PagerView>
             <CommentList
                 bottomSheetModalRef={bottomSheetModalRef}
                 firstCommentBundle={commentsBundles[0]}
