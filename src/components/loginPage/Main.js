4a0ba811e12299d76c5d0a0b28ba4ebacaf5f8c9
com9009 <com90009@gmail.com>
2022-07-07 23:07:18 +0900

    Login init

---
+++ src/components/loginPage/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const Main = () => {
+    return (
+        <View style={styles.main}>
+
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create(
+    {
+        main: {
+            flex: 1,
+            backgroundColor: 'white',
+            flexDirection: 'column',
+        },
+    }
+)
+
+export default Main;
